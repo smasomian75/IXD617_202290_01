7736d38d8708aa75bf7cfc60a3ff97a46030a75b
Hamilton <hamdiggy@gmail.com>
2022-09-19 15:43:57 -0700

    tabgroup added

--- examples/js/jquery.js
+++ examples/js/jquery.js
@@ -15,4 +15,16 @@ $(()=>{
             .siblings("dd").slideUp();
     });
 
+    /* TAB GROUP */
+    $(".tabgroup .tab").on("click", function(e) {
+        let id = $(this).index();
+
+        $(this).addClass("active")
+            .siblings().removeClass("active")
+
+        $(this).closest(".tabgroup")
+            .find(".content").eq(id).addClass("active")
+            .siblings().removeClass("active")
+    })
+
 });
